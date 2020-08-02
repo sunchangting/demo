af8ba0a716b880fe565bb806a61c0e7c17841358
sunchangting <1947383626@qq.com>
2020-08-02 16:26:20 +0800

    修改服务

--- src/main/java/com/example/demo/service/impl/DemoServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/DemoServiceImpl.java
@@ -9,5 +9,8 @@ import org.springframework.stereotype.Service;
  **/
 @Service
 public class DemoServiceImpl implements DemoService {
-
+    public void test(){
+        System.out.println("你好");
+    }
 }
+
