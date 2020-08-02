0a7208e8381edcc026fc1efbe6883872aaca5006
sunchangting <1947383626@qq.com>
2020-08-02 15:45:57 +0800

    新增demo服务

---
+++ src/main/java/com/example/demo/service/DemoService.java
@@ -0,0 +1,8 @@
+package com.example.demo.service;
+
+/**
+ * @author sunchangting
+ * @create 2020/8/2 14:48
+ **/
+public interface DemoService {
+}
---
+++ src/main/java/com/example/demo/service/impl/DemoServiceImpl.java
@@ -0,0 +1,13 @@
+package com.example.demo.service.impl;
+
+import com.example.demo.service.DemoService;
+import org.springframework.stereotype.Service;
+
+/**
+ * @author sunchangting
+ * @create 2020/8/2 14:49
+ **/
+@Service
+public class DemoServiceImpl implements DemoService {
+
+}
